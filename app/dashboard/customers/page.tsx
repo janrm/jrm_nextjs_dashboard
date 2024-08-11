2f39150b5b49fa4b62ac7e6a94e5ecf9b7f27f83
Jan Roelof <>
2024-08-11 19:01:55 +0200

    Initial Commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const CustomerPage = () => {
+    return (
+        <div>Customers Page</div>
+    )
+}
+export default CustomerPage
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const InvoicesPage = () => {
+    return (
+        <div>Invoices Page</div>
+    )
+}
+export default InvoicesPage
